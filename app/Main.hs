c8d60c522f5b011903862f41131113e6b147b994
oldflat <smJU.20tackom$eat>
2023-03-02 20:59:51 -0500

    learn you a haskell, very beginning

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,22 @@
 module Main where
+import Data.Bits (Bits(xor))
+import Control.Concurrent (yield)
 
 --import qualified MyLib (someFunc)
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
+  putStrLn "Hello, World!"
   --MyLib.someFunc
+
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+  then x
+  else x*2
+
+doubleSmallNumber' x = (if x > 100 then x else doubleMe x) + 1
+
