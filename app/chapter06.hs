de9b285eb4324934544682bffd2bb53e69cff52d
oldflat <smJU.20tackom$eat>
2023-03-05 12:56:00 -0500

    looking at map

--- app/chapter06.hs
+++ app/chapter06.hs
@@ -9,6 +9,10 @@ zipWith' _ [] _ = []
 zipWith' _ _ [] = []  
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys 
 
-
-
+{-
+    ghci> map (++ " asdf ") ["1", "2"]
+    ["1 asdf ","2 asdf "]
+    ghci> map (" asdf " ++) ["1", "2"]
+    [" asdf 1"," asdf 2"]
+-}
 
